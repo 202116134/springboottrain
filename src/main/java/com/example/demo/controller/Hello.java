a524cbea3d2c2fa07f69b6555ba0599c38285eaf
202116134 <15879876936@163.com>
2022-06-09 03:38:52 +0800

    test

---
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name",required = false,defaultValue = "默认王老五") String name ,Model model) {
+        model.addAttribute("name", name);
+        System.out.println("in hello controller");
+        return "hellopage";
+    }
+}
